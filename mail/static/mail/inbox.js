7e196fd2ba9e69441582d89def23f8cb542ac966
Fischer, Angelina <angelina.fischer@sva.de>
2023-05-30 14:52:26 +0200

    Add view email JS

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -18,7 +18,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  document.querySelector('#single-email-view').style.display = 'none';
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -26,11 +27,72 @@ function compose_email() {
   document.querySelector('#compose-error').innerHTML = ''
 }
 
+function load_email(mailbox, email_id) {
+  
+  // Show single email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'block';
+
+  // Get single-email-view element and clear content
+  const single_email = document.querySelector('#single-email-view');
+  single_email.innerHTML = '';
+
+  // Send GET request to /emails/<email_id> to request email
+  fetch(`emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+
+    // If error, show error
+    if ('error' in email) {
+      const error_div = document.createElement('div');
+      error_div.classList.add('error');
+      error_div.innerHTML = email['error'];
+      document.querySelector('#single-email-view').append(error_div);
+    }
+
+    // Show email header
+    const email_header_elements = [['From', 'sender'], ['To', 'recipients'], ['Subject', 'subject'], ['Timestamp', 'timestamp']]
+    email_header_elements.forEach(element => {
+      const element_title = element[0];
+      const element_content = element[1];
+      const element_row_div = document.createElement('div');
+      element_row_div.classList.add('row', `section-email-${element_content}`);
+      element_row_div.innerHTML = `<p><span class="section-email-title">${element_title}:</span> ${email[element_content]}</p>`
+
+      single_email.append(element_row_div);
+    });
+
+    // Separate email header and body
+    single_email.append(document.createElement('hr'));
+
+    // Show email body
+    const body_div = document.createElement('div');
+    body_div.classList.add('container', `section-email-body`);
+    body_div.innerHTML = `<p>${email.body}</p>`
+
+    single_email.append(body_div);
+  })
+  .catch(error => console.log('Error:', error)); // Catch error
+
+  // Send PUT request to /emails/<email_id> to mark email as read
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
+  .catch(error => console.log('Error:', error)); // Catch error
+}
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none';
 
   // Show which mailbox is selected
   document.querySelectorAll("button").forEach(button => button.classList.remove("selected"));
@@ -46,6 +108,14 @@ function load_mailbox(mailbox) {
     // Print emails
     console.log(emails);
 
+    // If error, show error
+    if ('error' in emails) {
+      const error_div = document.createElement('div');
+      error_div.classList.add('error');
+      error_div.innerHTML = email['error'];
+      document.querySelector('#emails-view').append(error_div);
+    }
+
     // Create email elements and show in view
     const email_sections = [['sender', 4], ['subject', 4], ['timestamp', 4]]
     emails.forEach(email => {
@@ -57,13 +127,16 @@ function load_mailbox(mailbox) {
         const section_name = section[0];
         const section_col_size = section[1];
         const div_section = document.createElement('div');
-        div_section.classList.add(`col-${section_col_size}`, `section-${section_name}`)
-        div_section.innerHTML = `<p>${email[section_name]}</p>`
+        div_section.classList.add(`col-${section_col_size}`, `section-${section_name}`);
+        div_section.innerHTML = `<p>${email[section_name]}</p>`;
 
         div_row_element.append(div_section);
       });
 
       document.querySelector('#emails-view').append(div_row_element);
+      
+      // Load single email when email row element is clicked
+      div_row_element.addEventListener('click', () => load_email(mailbox, email['id']));
     });
   })
   .catch(error => console.log('Error:', error)); // Catch error
@@ -86,13 +159,14 @@ function send_email() {
     console.log(result);
 
     // If error in result, show error message
-    if ("error" in result) {
-      document.querySelector('#compose-error').innerHTML = result['error'];
+    if ('error' in result) {
+      const error_div = document.querySelector('#compose-error');
+      error_div.classList.add('error')
+      error_div.innerHTML = result['error'];
     }
-    else {
-      // Show user's sent mailbox
-      load_mailbox('sent');
-    }
+    
+    // Show user's sent mailbox
+    load_mailbox('sent');
   })
   .catch(error => console.log('Error:', error)); // Catch error
 
